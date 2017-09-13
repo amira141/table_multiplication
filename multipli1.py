8a0b637bcdb3e157a3cd15ca2ba964a6d8ff09bc
Amira <amira141592@gmail.com>
2017-09-13 16:51:29 +0300

    ajout despaces

--- multipli1.py
+++ multipli1.py
@@ -2,9 +2,9 @@
 
 import os
 
-nb_u = input("Tapez un nombre pour obtenir sa table de multiplication")
+nb_u = input("Tapez un nombre pour obtenir sa table de multiplication"" ")
 nb = int(nb_u)
-MAX_u = input("Tapez le chiffre maximal pour lequel vous voulez obtenir sa table de multiplication")
+MAX_u = input("Tapez le chiffre maximal pour lequel vous voulez obtenir sa table de multiplication"" ")
 MAX = int(MAX_u)
 
 def table_multiplication(nb, MAX):
